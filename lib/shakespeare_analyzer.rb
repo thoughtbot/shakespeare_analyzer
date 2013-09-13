20aeacfbe160640b1c017f1a5fbd3b621c9956d0
Dave West <dave@twosevenzero.com>
2013-09-12 23:01:18 -0400

    wip

--- lib/shakespeare_analyzer.rb
+++ lib/shakespeare_analyzer.rb
@@ -1,3 +1,33 @@
+require 'net/http'
+require 'rexml/document'
+
 class ShakespeareAnalyzer
+
+  attr_reader :url
+
+  def initialize url = "http://www.ibiblio.org/xml/examples/shakespeare/macbeth.xml"
+    @url = url
+    @output = String.new
+  end
+
+  def run hash
+    hash.each do |name,number_of_lines|
+      @output << "#{number_of_lines}\t#{name}\n"
+    end
+
+    return @output
+  end
+
+  private
+
+  def fetch_raw_xml_data
+    Net::HTTP.get_response(URI.parse(@url)).body
+  end
+  
+  def create_rexml_document
+    @doc = REXML::Document.new(fetch_raw_xml_data)
+  end
+
+
 
 end
--- spec/shakespeare_analyzer_spec.rb
+++ spec/shakespeare_analyzer_spec.rb
@@ -2,10 +2,37 @@ require "spec_helper"
 
 describe ShakespeareAnalyzer do
 
- describe "new" do
+  before :all do
+    @shakespeare = ShakespeareAnalyzer.new
+    @xml_url = "http://www.ibiblio.org/xml/examples/shakespeare/macbeth.xml"
+  end
 
-    
+  describe "#new" do
 
- end
+    it "should be an instance of ShakespeareAnalyzer." do
+      expect(@shakespeare).to  be_an_instance_of ShakespeareAnalyzer
+    end
+
+    it "should have a default value for the url variable." do
+      expect(@shakespeare.url).to eql @xml_url
+    end
+
+    it "should allow the url value to be set upon initilization." do
+      hamlet = ShakespeareAnalyzer.new "hamlet.xml"
+      expect(hamlet.url).to eql "hamlet.xml"
+    end
+
+  end
+  
+  describe "#run" do
+
+    it "should print out the name and line count for each character." do
+      hash = { "Pi" => 314, "Life" => 42 }
+      run = ShakespeareAnalyzer.new.run hash
+      expect(run).to eql "314\tPi\n42\tLife\n"
+    end
+
+  end
+
 
 end
