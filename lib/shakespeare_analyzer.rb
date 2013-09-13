ef5eecefd4bbdf0fb2a159901496eeea2557dd08
Patrik Bóna <patrik.bona@mrhead.sk>
2013-09-13 10:28:34 +0200

    ShakespeareAnalyzer can print correct output parsed from XML file

--- lib/shakespeare_analyzer.rb
+++ lib/shakespeare_analyzer.rb
@@ -1,4 +1,5 @@
 require 'net/http'
+require 'xml_parser'
 
 class ShakespeareAnalyzer
   attr_accessor :file_content
@@ -7,9 +8,24 @@ class ShakespeareAnalyzer
    @file_content = get_content_from_uri(uri)
   end
 
+  def print_speakers_sorted_by_line_count
+    speakers_sorted_by_line_count.each do |speaker, lines|
+      puts "#{lines} #{speaker}"
+    end
+  end
+
   private
 
   def get_content_from_uri(uri)
     Net::HTTP.get_response(URI.parse(uri)).body
+  end
+
+  def speakers_sorted_by_line_count
+    speakers = {}
+    xml_parser = XmlParser.new(@file_content)
+    xml_parser.speakers.each do |speaker|
+      speakers[speaker] = xml_parser.lines_by_speaker(speaker)
+    end
+    speakers.sort_by { |_key, value| value }.reverse
   end
 end
--- spec/shakespeare_analyzer_spec.rb
+++ spec/shakespeare_analyzer_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 require 'shakespeare_analyzer'
+require 'stringio'
 
 describe ShakespeareAnalyzer do
   describe '#initialize' do
@@ -9,4 +10,24 @@ describe ShakespeareAnalyzer do
       expect(analyzer.file_content).to eq 'This is just a test file!'
     end
   end
+
+  describe '#print_speakers_sorted_by_line_count' do
+    it 'prints list of speakers sorted by line count' do
+      test_file = File.dirname(__FILE__) + '/test_files/test.xml'
+      stub_request(:get, 'http://www.example.com/test.xml').to_return(body: File.read(test_file))
+      analyzer = ShakespeareAnalyzer.new('http://www.example.com/test.xml')
+      output = capture_stdout { analyzer.print_speakers_sorted_by_line_count }
+      expect(output).to eq "3 ThreeLiner\n2 TwoLiner\n1 OneLiner\n"
+    end
+  end
+end
+
+def capture_stdout &block
+  old_stdout = $stdout
+  fake_stdout = StringIO.new
+  $stdout = fake_stdout
+  block.call
+  fake_stdout.string
+ensure
+  $stdout = old_stdout
 end
