87bbbf57abd88054a410d7a5f1ff72a92fa37242
Patrik Bóna <patrik.bona@mrhead.sk>
2013-09-13 21:10:06 +0200

    Extract `speakers` method in `ShakespeareAnalyzer`
    Almost all methods are now one line long.

--- lib/shakespeare_analyzer.rb
+++ lib/shakespeare_analyzer.rb
@@ -13,22 +13,22 @@ class ShakespeareAnalyzer
     open(uri, proxy: ENV['http_proxy']).read
   end
 
-  def speakers_sorted_by_line_count
-    speakers = {}
-    xml_parser.speakers.each do |speaker|
-      speakers[speaker] = xml_parser.count_lines_by_speaker(speaker)
-    end
-    speakers.sort_by { |_key, value| value }.reverse
-  end
-
-  def xml_parser
-    @_xml_parser ||= XmlParser.new(@file_content)
-  end
-
   def print_speakers_sorted_by_line_count
     speakers_sorted_by_line_count.each do |speaker, lines|
       puts "#{lines} #{speaker}"
     end
+  end
+
+  def speakers_sorted_by_line_count
+    speakers.sort_by { |_key, value| value }.reverse
+  end
+
+  def speakers
+    Hash[*xml_parser.speakers.map { |s| [s, xml_parser.count_lines_by_speaker(s)] }.flatten]
+  end
+
+  def xml_parser
+    @_xml_parser ||= XmlParser.new(@file_content)
   end
 end
 
