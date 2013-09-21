1a1e32247c43fc9ec1fdf77a754b05a18c60963a
Andy Waite <github.aw@andywaite.com>
2013-09-21 13:34:51 +0100

    Extract analyser into let to remove duplication

--- spec/macbeth_analyser_spec.rb
+++ spec/macbeth_analyser_spec.rb
@@ -2,19 +2,21 @@ require_relative '../macbeth_analyser'
 
 describe MacbethAnalyser do
   describe "#run" do
+    let(:analyser) { MacbethAnalyser.new(@xml) }
+
     it "returns an empty hash for an empty document" do
-      analyser = MacbethAnalyser.new("")
+      @xml = ""
+      #analyser = MacbethAnalyser.new("")
       expect(analyser.run).to eq({})
     end
 
     it "counts the lines in a fragment containing one speaker and one line" do
-      xml = """
+      @xml = """
         <SPEECH>
           <SPEAKER>Third Witch</SPEAKER>
           <LINE>That will be ere the set of sun.</LINE>
         </SPEECH>
       """
-      analyser = MacbethAnalyser.new(xml)
       expect(analyser.run).to eq({'Third Witch' => 1})
     end
   end
