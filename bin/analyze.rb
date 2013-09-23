f98f9198c1852c15b2f7ef51aca5467218304d50
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2013-09-22 17:24:18 -0700

    Fix analyzer to use new naming conventions

--- bin/analyze.rb
+++ bin/analyze.rb
@@ -4,5 +4,5 @@ require_relative '../lib/shakespeare_analyzer'
 require_relative '../lib/downloader'
 
 downloader = Downloader.new file: "http://www.ibiblio.org/xml/examples/shakespeare/macbeth.xml"
-shakes = ShakespeareAnalyzer.new downloader: downloader
+shakes = ShakespeareAnalyzer.new xml_provider: downloader
 shakes.print_lines
