42c76941578388a2ecf8bd2059b608237261e6dd
ankur kothari <ankothari@gmail.com>
2013-12-22 04:36:14 +0530

    deleted analyzer.rb

--- analyzer.rb
+++
@@ -1,8 +0,0 @@
-require_relative 'lib/http_content.rb'
-require_relative 'lib/shakespeare_analyzer'
-macbeth_url = "http://www.ibiblio.org/xml/examples/shakespeare/macbeth.xml"
-content = HttpContent.new(macbeth_url)
-analyzer = ShakespeareAnalyzer.new(content.data)
-analyzer.analyze
-result = analyzer.result
-analyzer.print_result
