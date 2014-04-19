0fc273ae2adc61989543f55bafb7a5971ec23ce3
MunkiPhD <munkiphd@gmail.com>
2014-04-19 10:35:40 -0400

    now scans a source file to read the characters and lines

--- lib/macbeth_analyzer.rb
+++ lib/macbeth_analyzer.rb
@@ -1,13 +1,33 @@
+require 'terminal_output'
+require 'nokogiri'
+
 class MacbethAnalyzer
-	def initialize()
+	def initialize(source)
+		@speakers = Hash.new
+		@xml_doc = Nokogiri::XML(source)
 		@output = TerminalOutput.new
+
 	end
 
 	def analyze
-		@output.add_new_line "12 Macbeth"
-		@output.add_new_line "4 Banquo"
-		@output.add_new_line "3 Duncan"
+		parse_speakers
+
+		# sort it so that they are in descending order by key (line count)
+		@speakers = Hash[@speakers.sort_by { |key, value| value }.reverse]
+
+		@speakers.each do |key, value|
+			@output.add_new_line "#{value} #{key.capitalize}"
+		end
 
 		@output.print
+	end
+
+	private 
+
+	def parse_speakers
+		@xml_doc.xpath("//SPEAKER").each do |node|
+			siblings = @xml_doc.search("SPEAKER[text()='#{node.content}'] ~ *").map &:text
+			@speakers[node.content] = siblings.length
+		end
 	end
 end
--- spec/macbeth_analyzer_spec.rb
+++ spec/macbeth_analyzer_spec.rb
@@ -2,9 +2,10 @@ require 'spec_helper'
 require_relative '../lib/macbeth_analyzer'
 
 describe 'displays number of total lines spoken by characters in Macbeth' do
-	it 'prints the number with the name' do
-		analyzer = MacbethAnalyzer.new
+	it 'prints the correct number of lines for the characters with the specified document' do
+		source = File.read('spec/sample_data.xml')
+		analyzer = MacbethAnalyzer.new(source)
 		results = analyzer.analyze
-		expect(results).to eq "12 Macbeth\n4 Banquo\n3 Duncan"
+		expect(results).to eq "11 Macbeth\n5 Malcolm\n2 All"
 	end
 end
