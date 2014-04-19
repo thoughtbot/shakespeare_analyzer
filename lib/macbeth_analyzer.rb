18f7e9602e1d3d5ac75aa8af66a5d3f15adfd782
MunkiPhD <munkiphd@gmail.com>
2014-04-18 22:12:04 -0400

    removed the output type and pushed it into the analyzer

--- lib/macbeth_analyzer.rb
+++ lib/macbeth_analyzer.rb
@@ -1,5 +1,5 @@
 class MacbethAnalyzer
-	def initialize(output)
+	def initialize(output = StringIO.new)
 		@output = output
 	end
 
@@ -7,5 +7,8 @@ class MacbethAnalyzer
 		@output.puts "12 Macbeth"
 		@output.puts "4 Banquo"
 		@output.puts "3 Duncan"
+
+		@output.seek(0)
+		@output.read.chomp
 	end
 end
--- spec/macbeth_analyzer_spec.rb
+++ spec/macbeth_analyzer_spec.rb
@@ -3,10 +3,8 @@ require_relative '../lib/macbeth_analyzer'
 
 describe 'displays number of total lines spoken by characters in Macbeth' do
 	it 'prints the number with the name' do
-		output = StringIO.new
-		analyzer = MacbethAnalyzer.new(output)
-		analyzer.analyze
-		output.seek(0)
-		expect(output.read.chomp).to eq "12 Macbeth\n4 Banquo\n3 Duncan"
+		analyzer = MacbethAnalyzer.new
+		results = analyzer.analyze
+		expect(results).to eq "12 Macbeth\n4 Banquo\n3 Duncan"
 	end
 end
